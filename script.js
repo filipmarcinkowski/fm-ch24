cebd4f30622ae233c3aeef07e1bf59b4c49b0b61
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-03-19 19:57:26 +0100

    async included

--- script.js
+++ script.js
@@ -6,14 +6,28 @@ const btn = document.querySelector('.btn');
 
 const URL = 'https://api.adviceslip.com/advice';
 
-const showAdvice = function () {
-  fetch(URL)
-    .then((res) => res.json())
-    .then((data) => {
-      advice.textContent = data.slip.advice;
-      adviceNum.textContent = data.slip.id;
-    })
-    .catch((err) => console.error(err));
+// const showAdvice = function () {
+//   fetch(URL)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       advice.textContent = data.slip.advice;
+//       adviceNum.textContent = data.slip.id;
+//     })
+//     .catch((err) => console.error(err));
+// };
+
+// btn.addEventListener('click', showAdvice);
+
+const showAdvice2 = async function () {
+  try {
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    advice.textContent = data.slip.advice;
+    adviceNum.textContent = data.slip.id;
+  } catch {
+    console.error('error');
+  }
 };
 
-btn.addEventListener('click', showAdvice);
+btn.addEventListener('click', showAdvice2);
