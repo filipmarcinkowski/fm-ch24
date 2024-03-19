418eb8564771b248b93fb2600ae2370d379e51c6
filipmarcinkowski <filipmarcinkowski26@gmail.com>
2024-03-19 17:34:55 +0100

    js done with fetch

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+'use strict';
 
+const adviceNum = document.querySelector('.id');
+const advice = document.querySelector('.advice');
+const btn = document.querySelector('.btn');
+
+const URL = 'https://api.adviceslip.com/advice';
+
+const showAdvice = function () {
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      advice.textContent = data.slip.advice;
+      adviceNum.textContent = data.slip.id;
+    })
+    .catch((err) => console.error(err));
+};
+
+btn.addEventListener('click', showAdvice);
